4812c6337c7d5d2b03f4e5032614ff53b03c6ed4
Murilo Sanches <murilosanches@live.com>
2024-07-16 11:00:16 -0300

    Quarto Commit

---
+++ Mundo 01/Manipulando Texto/aula009.1.py
@@ -0,0 +1,20 @@
+frase = "Curso em Vídeo Python"
+frase2 = "   Aprenda Python  "
+print(frase[:2])
+#len(frase) | contar os caracteres
+#frase.count("o") | contar quantos 'o' tem na variante
+#frase.count("o,0,13") | contar quantos 'o' tem na variante com fatiamento
+#frase.find("deo") | mostra em quais números estão alocados 'deo' na variante
+#frase.find("Android") | retorna o valor -1, pois a string 'Android' não existe
+#"Curso" in frase | verifica se a palavra 'Curso' existe na variante, retorna true
+#frase.replace("Python","Android") | substitui a palavra existente pela nova "Python" -> "Android"
+#frase.upper() | transforma todas as letras para maiúsculas
+#frase.lower() | transforma todas as letras para minúsculas
+#frase.capitaliza() | transforma todas as letras para minúscula, com excessão da primeira, que mantém maiúscula
+#frase.title() | igual o 'capitaliza', porém, quando houver espaços, a próxima letra será maiúscula.
+#frase2.strip() | remove os espaços antes do inicio e espaços após o final.
+#frase2.rstrip() | remove somente os últimos espaços
+#frase2.lstrip() | remove somente os primeiros espaços
+#frase.split() | realiza uma divisão do fatiamento, conforme tiver espaços.
+#'-'.join(frase) | substitui os espaços pelos traço -
+
