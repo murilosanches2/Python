624a9dd797d3e2f5d7b23af4769dc93a179af980
Murilo Sanches <murilosanches@live.com>
2024-11-10 20:23:57 -0300

    a

--- AgendaContatos/Agenda de Contatos/src/interfaces/InterfaceControle.java
+++
@@ -1,8 +0,0 @@
-
-package interfaces;
-
-public interface InterfaceControle {
-    public void salvarControle(Object... valor);
-    public void excluirControle(int id);
-    public void carregarComboBox();
-}
